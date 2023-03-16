af05e1a3fe910eb832010276a8de9f640cddcb5b
Topher L <topher@meanservers.com>
2023-03-16 17:23:43 +0100

    tailwind-3 from bootstrap-4

--- resources/views/tailwind-3/table.blade.php
+++ resources/views/tailwind-3/table.blade.php
@@ -155,8 +155,10 @@
                                        title="{{ $sortDir === 'asc' ? __('Sort descending') : __('Sort ascending') }}"
                                        data-toggle="tooltip">
                                         {!! $sortDir === 'asc'
-                                            ? config('laravel-table.icon.sort_desc')
-                                            : config('laravel-table.icon.sort_asc') !!}
+                                            ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+  <path fill-rule="evenodd" d="M11.47 7.72a.75.75 0 011.06 0l7.5 7.5a.75.75 0 11-1.06 1.06L12 9.31l-6.97 6.97a.75.75 0 01-1.06-1.06l7.5-7.5z" clip-rule="evenodd" />
+</svg>'
+                                            : '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path fill-rule="evenodd" d="M12.53 16.28a.75.75 0 01-1.06 0l-7.5-7.5a.75.75 0 011.06-1.06L12 14.69l6.97-6.97a.75.75 0 111.06 1.06l-7.5 7.5z" clip-rule="evenodd" /> </svg>' !!}
                                         <span class="ml-2">{{ $column->getTitle() }}</span>
                                     </a>
                                 @else
@@ -165,7 +167,7 @@
                                        href=""
                                        title="{{ __('Sort ascending') }}"
                                        data-toggle="tooltip">
-                                        {!! config('laravel-table.icon.sort') !!}
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6"> <path fill-rule="evenodd" d="M11.47 4.72a.75.75 0 011.06 0l3.75 3.75a.75.75 0 01-1.06 1.06L12 6.31 8.78 9.53a.75.75 0 01-1.06-1.06l3.75-3.75zm-3.75 9.75a.75.75 0 011.06 0L12 17.69l3.22-3.22a.75.75 0 111.06 1.06l-3.75 3.75a.75.75 0 01-1.06 0l-3.75-3.75a.75.75 0 010-1.06z" clip-rule="evenodd" /> </svg>
                                         <span class="ml-2">{{ $column->getTitle() }}</span>
                                     </a>
                                 @endif
